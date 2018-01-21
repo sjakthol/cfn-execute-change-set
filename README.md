644e811985ab5655e54d652f788adb72030a0aaa
Sami Jaktholm <sjakthol@outlook.com>
2018-01-21 15:36:27 +0200

    initial implementation

---
+++ README.md
@@ -0,0 +1,68 @@
+A command line interface for reviewing and executing AWS CloudFormation changesets.
+
+## Features
+
+* Read CloudFormation change set ARN from stdin or from the command line
+* Print a summary of changes to stack resources, parameters and tags
+* View the chain of causes that leads to resource changes (experimental)
+* Review changes and execute them right away
+
+**Note**: Resource change cause chians are experimental and might not be fully
+accurate. Always review the raw change set input if you are unsure how changes
+propagate to resources.
+
+## Installation
+```
+npm i -g cfn-execute-change-set
+```
+
+## Usage
+
+To review and execute a changeset, just pipe the output of the awscli
+`create-change-set` command to the `cfn-execute-change-set` tool:
+
+```
+aws cloudformation create-change-set [...] | cfn-execute-change-set
+```
+
+Alternatively, you can provide a CloudFormation change set ARNs as command line
+argument:
+
+```bash
+cfn-execute-change-set \
+    arn:aws:cloudformation:eu-west-1:123456789123:changeSet/test-1516522726/9957ed5e-0049-4144-bc82-962941d972e4
+```
+
+### Example
+```
+$ aws cloudformation create-change-set [] | cfn-execute-change-set
+{
+    "StackId": "arn:aws:cloudformation:eu-west-1:748888633826:stack/ew1-test/e0496e10-fe10-11e7-8420-50fae9b818d2",
+    "Id": "arn:aws:cloudformation:eu-west-1:748888633826:changeSet/test-1516540773/0e3d729c-9712-43aa-a7d3-2b44e4d2b797"
+}
+Changeset is being created. Waiting...
+Changeset is being created. Waiting...
+Resource Changes
+[*] AuthenticatorLambda - ew1-test-AuthenticatorLambda-113XFFG3TVL7D (AWS::Lambda::Function)
+    - resource tags changed
+        caused by changed stack tags
+[*] DataBucket - cfn-execute-changeset-test-1 (AWS::S3::Bucket)
+    - resource tags changed
+        caused by changed stack tags
+[*] FirehoseDeliveryStream - ew1-test-FirehoseDeliveryStream-115L2LE9AKCXP (AWS::KinesisFirehose::DeliveryStream) [Replacement: Conditional]
+    - resource property ExtendedS3DestinationConfiguration might change [Recreation: Always]
+        caused by changed output value of AuthenticatorLambda.Arn
+        caused by changed stack tags
+
+Tag Changes
+[*] Another: test1 ⟶ test
+
+Execute change set [y/N]? y
+Stack update started:
+- Change Set ARN: arn:aws:cloudformation:eu-west-1:748888633826:changeSet/test-1516540773/0e3d729c-9712-43aa-a7d3-2b44e4d2b797
+- Stack ARN: arn:aws:cloudformation:eu-west-1:748888633826:stack/ew1-test/e0496e10-fe10-11e7-8420-50fae9b818d2
+```
+
+## Ideas / TODO
+* Improve and clean up code that determines root cause for resource changes
+* More tests
---
+++ index.js
@@ -0,0 +1,158 @@
+#!/usr/bin/env node
+
+const readline = require('readline')
+
+const chalk = require('chalk')
+const ttys = require('ttys')
+
+const analysis = require('./lib/analysis')
+const cfn = require('./lib/cfn')
+const helpers = require('./lib/helpers')
+const renderers = require('./lib/renderers')
+
+let found = false
+
+/**
+ * Checks the given input for CloudFormation changeset ARN and
+ * starts review if one is found.
+ *
+ * @param {String} input the input to parse
+ */
+function maybeReviewChangeSet (input) {
+  if (found) {
+    return
+  }
+
+  const info = helpers.getChangeSetInfoFromInput(input)
+  if (!info) {
+    return
+  }
+
+  found = true
+
+  cfn.describeChangeSet(info, function (err, data) {
+    if (err) throw err
+
+    const resourceChanges = analysis.analyzeResourceChanges(data.changeset)
+    printResourceChanges(resourceChanges)
+
+    const tagChanges = analysis.analyzeTagChanges(data.changeset, data.stack)
+    printKeyValueChanges(tagChanges, 'Tag Changes')
+
+    const parameterChanges = analysis.analyzeParameterChanges(data.changeset, data.stack)
+    printKeyValueChanges(parameterChanges, 'Parameter Changes')
+
+    promptAndExecuteChanges(data.changeset)
+  })
+}
+
+/**
+ * @typedef KeyValueChange
+ * @property {String} Key
+ * @property {String} Value
+ * @property {String} [OldValue]
+ */
+
+/**
+ * @typedef KeyValueChanges
+ * @property {KeyValueChange[]} added
+ * @property {KeyValueChange[]} removed
+ * @property {KeyValueChange[]} modified
+ */
+
+/**
+ * @param {KeyValueChanges} changes
+ */
+function printKeyValueChanges (changes, title) {
+  if (!changes.added.length && !changes.removed.length && !changes.modified.length) {
+    // No tag changes!
+    return
+  }
+
+  console.log() // Print empty line to separate this from previous section
+  console.log(chalk.bold(title))
+  /**
+   * @param {String} action
+   * @param {KeyValueChange[]} changes
+   */
+  function _printKeyValueChanges (action, changes) {
+    for (let change of changes) {
+      const type = renderers.renderAction({ Action: action })
+      const summary = renderers.renderKeyValueChange(change)
+      console.log(`${type} ${summary}`)
+    }
+  }
+
+  _printKeyValueChanges('Add', changes.added)
+  _printKeyValueChanges('Remove', changes.removed)
+  _printKeyValueChanges('Modify', changes.modified)
+}
+
+/**
+ * @param {Object} changes
+ * @param {AWS.CloudFormation.ResourceChange[]} changes.added
+ * @param {AWS.CloudFormation.ResourceChange[]} changes.removed
+ * @param {AWS.CloudFormation.ResourceChange[]} changes.modified
+ */
+function printResourceChanges (changes) {
+  /**
+   * @param {AWS.CloudFormation.ResourceChange[]} changes
+   */
+  function _printResources (changes) {
+    for (let change of changes) {
+      const type = renderers.renderAction(change)
+      const resource = renderers.renderResourceSummary(change)
+      const replacement = renderers.renderReplacement(change)
+      console.log(`${type} ${resource}${replacement}`)
+
+      for (let detail of change.Details) {
+        const recreation = renderers.renderRecreation(detail)
+        console.log(`    - ${detail.Summary}${recreation}`)
+        for (let cause of (detail.Causes || [])) {
+          console.log(`        caused by ${cause}`)
+        }
+      }
+    }
+  }
+
+  console.log(chalk.bold('Resource Changes'))
+  _printResources(changes.added)
+  _printResources(changes.removed)
+  _printResources(changes.modified)
+}
+
+/**
+ * @param {AWS.CloudFormation.DescribeChangeSetOutput} changeset
+ */
+function promptAndExecuteChanges (changeset) {
+  const i = readline.createInterface(ttys.stdin, ttys.stdout)
+  console.log() // Empty line
+  i.question('Execute change set [y/N]? ', function (answer) {
+    if (answer === 'y') {
+      console.log('Executing change set...')
+      cfn.executeChangeSet(changeset)
+    } else {
+      console.log('Skipping change set execution')
+      process.exit(0)
+    }
+  })
+}
+
+// See if we have a changeset ARN(s) given as command line argument(s)
+if (process.argv.length > 2) {
+  maybeReviewChangeSet(process.argv[2])
+}
+
+// Also check if we have been piped some input and detect changeset
+// IDs from there
+if (!process.stdin.isTTY) {
+  const rl = readline.createInterface({
+    input: process.stdin
+  })
+
+  rl.on('line', line => {
+    // echo to stdout
+    console.log(line)
+    maybeReviewChangeSet(line)
+  })
+}
